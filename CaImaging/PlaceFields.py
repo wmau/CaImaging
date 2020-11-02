d7abd264e258d49d94a299dba0b1bed767e190fb
Will Mau <wmau@bu.edu>
2020-11-02 12:26:08 -0500

    Spatial information in bits/spk

--- CaImaging/PlaceFields.py
+++ CaImaging/PlaceFields.py
@@ -21,35 +21,60 @@ class PlaceFields:
         self.one_dim = one_dim
         self.bin_size = bin_size
 
-        self.make_occupancy_map(plot=False)
+        self.occupancy_map = self.make_occupancy_map(show_plot=False)
         self.pfs = self.make_all_place_fields()
 
 
     def make_all_place_fields(self):
         """
         Compute the spatial rate maps of all neurons.
-        
+
         :return:
         """
         pfs = []
         for neuron in range(self.neural_data.shape[0]):
-            pfs.append(self.make_place_field(neuron, plot=False))
+            pfs.append(self.make_place_field(neuron, show_plot=False))
 
         return pfs
 
+
+    def assess_stat_sig(self):
+        SI = []
+        SI2 = []
+        for pf in self.pfs:
+            SI.append(spatial_information(pf, self.occupancy_map))
+            SI2.append(spatial_information2(pf, self.occupancy_map))
+
+        fig, ax = plt.subplots()
+        ax.scatter(SI, SI2)
+        lims = [
+            np.min([ax.get_xlim(), ax.get_ylim()]),
+            # min of both axes
+            np.max([ax.get_xlim(), ax.get_ylim()]),
+            # max of both axes
+        ]
+        ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+        ax.set_aspect('equal')
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+        for i, (x, y) in enumerate(zip(SI, SI2)):
+            ax.annotate(i, (x, y))
+        fig.show()
+
+        pass
 
     def plot_dots(
         self, neuron, std_thresh=2, pos_color="k", transient_color="r", ax=None
     ):
         """
-        Plots a dot plot. Position samples with suprathreshold activity
+        Plots a dot show_plot. Position samples with suprathreshold activity
         dots overlaid.
 
         :parameters
         ---
         neuron: int, neuron index in neural_data.
         std_thresh: float, number of standard deviations above the mean
-            to plot "spike" dot.
+            to show_plot "spike" dot.
         pos_color: color-like, color to make position samples.
         transient_color: color-like, color to make calcium transient-associated
             position samples.
@@ -68,27 +93,31 @@ class PlaceFields:
         ax.scatter(self.x, self.y, s=3, c=pos_color)
         ax.scatter(self.x[supra_thresh], self.y[supra_thresh], s=3, c=transient_color)
 
-    def make_occupancy_map(self, plot=True, ax=None):
+    def make_occupancy_map(self, show_plot=True, ax=None):
         """
         Makes the occupancy heat cell_map of the animal.
 
         :parameters
         ---
         bin_size_cm: float, bin size in centimeters.
-        plot: bool, flag for plotting.
+        show_plot: bool, flag for plotting.
 
         """
-        self.occupancy_map = spatial_bin(
-            self.x, self.y, bin_size_cm=self.bin_size, plot=plot, one_dim=self.one_dim
+        occupancy_map = spatial_bin(
+            self.x, self.y, bin_size_cm=self.bin_size, show_plot=show_plot, one_dim=self.one_dim
         )[0]
 
-        if plot:
+        if show_plot:
             if ax is None:
                 fig, ax = plt.subplots()
 
-            ax.imshow(self.occupancy_map, origin="lower")
+            ax.imshow(occupancy_map, origin="lower")
 
-    def make_place_field(self, neuron, plot=True, ax=None):
+        return occupancy_map
+
+    def make_place_field(self, neuron, show_plot=True,
+                         normalize_by_occ=False,
+                         ax=None):
         """
         Bins activity in space. Essentially a 2d histogram weighted by
         neural activity.
@@ -97,7 +126,7 @@ class PlaceFields:
         ---
         neuron: int, neuron index in neural_data.
         bin_size_cm: float, bin size in centimeters.
-        plot: bool, flag for plotting.
+        show_plot: bool, flag for plotting.
 
         :return
         ---
@@ -107,14 +136,16 @@ class PlaceFields:
             self.x,
             self.y,
             bin_size_cm=self.bin_size,
-            plot=False,
+            show_plot=False,
             weights=self.neural_data[neuron],
             one_dim=self.one_dim,
         )[0]
 
         # Normalize by occupancy.
-        pf = pf / self.occupancy_map
-        if plot:
+        if normalize_by_occ:
+            pf = pf / self.occupancy_map
+
+        if show_plot:
             if ax is None:
                 fig, ax = plt.subplots()
 
@@ -122,6 +153,64 @@ class PlaceFields:
 
         return pf
 
+# Adrien Peyrache's formula.
+# def spatial_information(tuning_curve, occupancy):
+#     tuning_curve = tuning_curve.flatten()
+#     occupancy = occupancy.flatten()
+#
+#     occupancy = occupancy/np.sum(occupancy)
+#     f = np.sum(occupancy*tuning_curve)
+
+#     # This line is Matlab code. How do you do this in Python?
+#     # Note: not element-wise division.
+#     tuning_curve = tuning_curve/f
+
+#     idx = tuning_curve > 0
+#     SB = (occupancy[idx]*tuning_curve[idx])*np.log2(tuning_curve[idx])
+#     SpatialBits = np.sum(SB)
+#
+#     return SpatialBits
+
+
+def spatial_information(tuning_curve, occupancy):
+    """
+    Calculate spatital information in one neuron's activity.
+
+    :parameters
+    ---
+    tuning_curve: array-like
+        Activity (S or binary S) per spatial bin.
+
+    occupancy: array-like
+        Time spent in each spatial bin.
+
+    :return
+    ---
+    spatial_bits_per_spike: float
+        Spatial bits per spike.
+    """
+    # Make 1-D.
+    tuning_curve = tuning_curve.flatten()
+    occupancy = occupancy.flatten()
+
+    # Only consider activity in visited spatial bins.
+    tuning_curve = tuning_curve[occupancy > 0]
+    occupancy = occupancy[occupancy > 0]
+
+    # Find rate and mean rate.
+    rate = tuning_curve/occupancy
+    mrate = tuning_curve.sum()/occupancy.sum()
+
+    # Get occupancy probability.
+    prob = occupancy/occupancy.sum()
+
+    # Handle log2(0).
+    index = rate > 0
+
+    # Spatial information formula.
+    bits_per_spk = sum(prob[index] * (rate[index]/mrate) * np.log2(rate[index]/mrate))
+
+    return bits_per_spk
 
 if __name__ == "__main__":
     mouse = "G132"
