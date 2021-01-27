062689f9d5e40b0ad9182f9429678164b8596d1e
Will Mau <wmau@bu.edu>
2021-01-27 16:30:56 -0500

    Moved errorfill to plotting

--- CaImaging/plotting.py
+++ CaImaging/plotting.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
+
 from CaImaging.util import check_attrs
 
 
@@ -19,3 +21,31 @@ def overlay_footprints(ScrollObj):
     ax.imshow(overlay, origin="lower")
 
     ScrollObj.last_position = footprints[0].shape[0]
+
+
+def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None):
+    """
+    Line show_plot with error bars except the error bars are filled in
+    rather than the monstrosity from matplotlib.
+
+    :parameters
+    ---
+    x: array-like
+        x-axis values.
+
+    y: array-like, same length as x
+        y-axis values.
+
+    yerr: array-like, same length as x and y
+        Error around the y values.
+    """
+    ax = ax if ax is not None else plt.gca()
+    if color is None:
+        color = ax._get_lines.get_next_color()
+    if np.isscalar(yerr) or len(yerr) == len(y):
+        ymin = y - yerr
+        ymax = y + yerr
+    elif len(yerr) == 2:
+        ymin, ymax = yerr
+    ax.plot(x, y, color=color)
+    ax.fill_between(x, ymax, ymin, color=color, alpha=alpha_fill)
--- CaImaging/util.py
+++ CaImaging/util.py
@@ -825,34 +825,6 @@ def sem(arr, axis=0):
     return standard_error
 
 
-def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None):
-    """
-    Line show_plot with error bars except the error bars are filled in
-    rather than the monstrosity from matplotlib.
-
-    :parameters
-    ---
-    x: array-like
-        x-axis values.
-
-    y: array-like, same length as x
-        y-axis values.
-
-    yerr: array-like, same length as x and y
-        Error around the y values.
-    """
-    ax = ax if ax is not None else plt.gca()
-    if color is None:
-        color = ax._get_lines.color_cycle.next()
-    if np.isscalar(yerr) or len(yerr) == len(y):
-        ymin = y - yerr
-        ymax = y + yerr
-    elif len(yerr) == 2:
-        ymin, ymax = yerr
-    ax.plot(x, y, color=color)
-    ax.fill_between(x, ymax, ymin, color=color, alpha=alpha_fill)
-
-
 def chunk(lst, n):
     chunked = []
     for i in range(0, len(lst), n):
